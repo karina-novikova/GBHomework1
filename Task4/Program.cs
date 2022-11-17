c0895eb91e07d4210b49496e525d8c6ae0ffc6ca
Karina <karina.a.s@mail.ru>
2022-11-17 17:12:26 +0300

    Add task 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+
+int i = 1;
+bool dec = true;
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (i <= num){
+    if (i % 2 != 1){
+        Console.Write(i + ", ");
+        dec = false;
+    }
+i++;
+}
+if (dec)
+{
+Console.WriteLine("Нет чётных чисел!");
+}
