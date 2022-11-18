2d79fea419ed9da35310e3a78ef36fe1dd9d66a0
Karina <karina.a.s@mail.ru>
2022-11-18 17:04:43 +0300

    add task6 files

---
+++ Task6/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет.
+
+Console.Write("Введите любое число: ");
+int count;
+int num = Convert.ToInt32(Console.ReadLine());
+int numDigit = NumberDigit(num);// Пробуем функцию/метод.
+if (numDigit <= 2)
+{
+Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+if (numDigit > 3)
+{
+num = num / Convert.ToInt32(Math.Pow(10, numDigit - 3)); // немного Math
+}
+num = num % 10;
+Console.WriteLine($"Третья цифра введённого числа: {num}");
+ }
+
+int NumberDigit(int number)
+{
+    count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
