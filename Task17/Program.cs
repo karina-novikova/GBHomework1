e4e66091554c24441460878e7ae686a51269de97
Karina <karina.a.s@mail.ru>
2022-11-25 18:42:34 +0300

    Add Task 17

---
+++ Task17/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+Console.WriteLine($"Задача 41. Cколько чисел больше 0 ввёл пользователь \n");
+Console.Write($"Введи число М(количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = new int[m];
+
+void InputNumbers(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    arrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comparison(int[] arrayNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < arrayNumbers.Length; i++)
+  {
+    if(arrayNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(arrayNumbers)} ");
